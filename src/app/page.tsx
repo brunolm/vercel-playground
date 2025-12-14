a76443ea5f5c4318addb92587279c96c29354b51
Bruno Leonardo Michels <brunolm7@gmail.com>
2025-12-14 09:33:57 -0300

    click test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import { useCallback, useState } from "react";
+
 /** biome-ignore-all lint/suspicious/noExplicitAny: bla */
 export default async function Home() {
   return (
@@ -16,9 +19,27 @@ ${JSON.stringify(navigator.languages, null, 2)}
 ${JSON.stringify(navigator.language, null, 2)}
 `;
 
+  const [clickStatus, setClickStatus] = useState("");
+  const handleClick = useCallback((e) => {
+    setClickStatus(`
+isTrusted: ${e.isTrusted}
+pageX: ${e.pageX}
+pageY: ${e.pageY}
+screenX: ${e.screenX}
+screenY: ${e.screenY}
+clientX: ${e.clientX}
+clientY: ${e.clientY}
+`);
+  }, []);
+
   return (
     <div className="w-full max-w-xs">
       <pre>{txt}</pre>
+
+      <button id="clickHere" type="button" onClick={handleClick}>
+        Click here
+      </button>
+      <pre>{clickStatus}</pre>
     </div>
   );
 }
