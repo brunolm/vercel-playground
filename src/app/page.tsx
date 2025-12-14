8cbb4e3a94e184def8746d6e9d15e3dde478d22d
Bruno Leonardo Michels <brunolm7@gmail.com>
2025-12-14 09:44:39 -0300

    ba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import { Bla } from "./_components/bla";
 
 export default async function Home() {
   return (
-    <main className="mx-1 flex min-h-screen flex-col items-start bg-black text-2xl text-white">
-      <Bla />
+    <main className="flex min-h-screen flex-col items-start bg-black text-2xl text-white">
+      <div className="mx-1">
+        <Bla />
+      </div>
     </main>
   );
 }
