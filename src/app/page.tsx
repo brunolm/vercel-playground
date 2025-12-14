0a1ae28b203cd1f0dd4bb89bd87d98e05c9768f9
Bruno Leonardo Michels <brunolm7@gmail.com>
2025-12-14 09:46:14 -0300

    ba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Bla } from "./_components/bla";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start bg-black text-2xl text-white">
-      <div className="mx-1">
+      <div className="mx-3 flex-1">
         <Bla />
       </div>
     </main>
