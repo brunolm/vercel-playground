d4a86be8b2da64bd79aadec4f46d018dfea28223
Bruno Leonardo Michels <brunolm7@gmail.com>
2025-12-14 09:42:31 -0300

    bababa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Bla } from "./_components/bla";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-center bg-black text-sm text-white">
+    <main className="flex min-h-screen flex-col items-start bg-black text-2xl text-white">
       <Bla />
     </main>
   );
